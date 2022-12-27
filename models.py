b2443af23db7024d27c56d210067d980c683ffaf
Pavleosha <vostryakovpav@yandex.ru>
2022-12-27 21:07:18 +0300

    Oleg, sorry i just predstavil myself a programist

--- models.py
+++ models.py
@@ -1,1 +1,21 @@
-xxx
+class MaterialPoint:
+    def __init__(self, i, crd_x, crd_y, vel_x, vel_y, x0, y0, t):
+        self.i = i
+        self.crd_x = crd_x
+        self.crd_y = crd_y
+        self.vel_x = vel_x
+        self.vel_y - vel_y
+        self.x0 = x0
+        self.y0 = y0
+        self.t = t
+
+class MaterialBody:
+    def __init__(self, material_points):
+        self.material_points = material_points
+
+
+class PointTrajectory:
+    def __init__(self, material_point, x, y):
+        self.material_point = material_point
+        self.x = x
+        self.y = y
