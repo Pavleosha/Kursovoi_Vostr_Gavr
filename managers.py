69c4aec5a843a4c55d05a055fe7761132b6eb964
gavrilovOO <o301102gavrik@yandex.ru>
2022-12-28 14:27:23 +0300

    Update managers.py

--- managers.py
+++ managers.py
@@ -1,1 +1,12 @@
-# тут будут функции
+# Импорт модуля (модели) и библиотек
+import models.models as model
+import matplotlib.pyplot as plotlib
+import numpy as np
+
+# Задаём V1 и V2
+def f_x(t, x):
+    return - np.exp(t) * x
+
+
+def f_y(t, y):
+    return np.exp(t) * y
