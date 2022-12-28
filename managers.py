692ae5201e383b27673ba35657199de0ebece891
gavrilovOO <o301102gavrik@yandex.ru>
2022-12-28 15:10:17 +0300

    Update managers.py

--- managers.py
+++ managers.py
@@ -36,3 +36,14 @@ def cr_material_body(x0, y0, r, n):
     material_body = model.MaterialBody(material_points)
     return material_body
 
+# Вводим метод Рунге-Кутты (явный)
+def runge_method(x0, h, n, func, a, b, c):    # h-величина шага сетки, a,b,c - эл-ты таблицы Бутчера
+    xt = [x0]
+    t = 0
+    for i in range(n):
+        xn = xt[i]
+        k1 = func(t, xn)    # 1я стадия
+        k2 = func(t + c[2] * h, xn + a[2, 1] * h * k1)  # 2я стадия
+        xt.append(xn + h * (k1 * b[1] + k2 * b[2]))     # по общей формуле
+        t += h
+    return xt
