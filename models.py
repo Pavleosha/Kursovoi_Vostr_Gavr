f197a8822921d6b54530ff64dfac7dfd3b61c6c5
Pavleosha <vostryakovpav@yandex.ru>
2022-12-28 13:51:42 +0300

    Update models.py

--- models.py
+++ models.py
@@ -27,5 +27,17 @@ class BodyTrajectory:
         self.point_trajectories = point_trajectories
         self.material_body = material_body
 
+
 class SpacePoint:
-    def __init__(self, i ):
+    def __init__(self, i, crd_x, crd_y, vel_x, vel_y, t):
+        self.i = i
+        self.crd_x = crd_x
+        self.crd_y = crd_y
+        self.vel_x = vel_x
+        self.vel_y = vel_y
+        self.t = t
+
+
+class SpaceGrid:
+    def __init__(self, space_points):
+        self.space_points = space_points
